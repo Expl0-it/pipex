98bb5c8d81f441b5d98243dccda18f1c42dc0270
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-03 11:51:31 +0200

    started prototyping main

--- src/main.c
+++ src/main.c
@@ -1,36 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/17 17:51:07 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/03 11:29:50 by mamichal         ###   ########.fr       */
+/*   Created: 2024/08/03 11:30:02 by mamichal          #+#    #+#             */
+/*   Updated: 2024/08/03 11:51:02 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../includes/pipex.h"
 
-// INCLUDES
-# include "../libft/libft.h"
-# include <sys/types.h>
-
-typedef enum e_errors
+int	main(int argc, char **argv)
 {
-	OK = 0,
-}				t_errors;
-
-typedef struct s_pipex
-{
-	int		in_fd;
-	int		out_fd;
-	int		cmd_count;
-	bool	here_doc;
-	bool	invalid_file;
-	char	**cmd_paths;
-	char	***cmd_args;
-}				t_pipex;
-
-#endif
+	t_pipex	data;
+	
+}
