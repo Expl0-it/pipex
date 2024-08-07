1bf098c4075eeb82b3e1de31a680250f1e009275
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-07 16:24:22 +0200

    moved statements in init_pipex to avoid mistakes later

--- src/main.c
+++ src/main.c
@@ -6,21 +6,20 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 11:30:02 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/07 13:55:20 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/08/07 14:04:42 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-// @brief init with default values
 static void	init_pipex(t_pipex *p_pipex, char **argv)
 {
-	p_pipex->in_fd = -1;
-	p_pipex->out_fd = -1;
 	if (ft_strncmp(argv[1], "here_doc", 9))
 		p_pipex->here_doc = true;
 	else
 		p_pipex->here_doc = false;
+	p_pipex->in_fd = -1;
+	p_pipex->out_fd = -1;
 	p_pipex->invalid_infile = false;
 	p_pipex->urandom = false;
 	p_pipex->cmd_paths = NULL;
