3b052d9061352bcb373d0cde41fbb2987488db48
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-16 13:58:46 +0200

    added a funct to handle urandom as argument

--- src/files.c
+++ src/files.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 19:44:56 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/16 13:42:02 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/08/16 13:58:23 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,17 +56,38 @@ int	handle_here_doc(char *limiter)
 	return (0);
 }
 
+int	handle_urandom(void)
+{
+	int		tmp_fd;
+	int		urandom_fd;
+	char	*line;
+
+	urandom_fd = open("/dev/urandom", O_RDONLY);
+	if (urandom_fd < 0)
+		return (-1);
+	tmp_fd = open(URANDOM_OUT_PATH, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (tmp_fd < 0)
+		return  (-1);
+	if (-1 == read_line(&line, urandom_fd, 0))
+		return (-1);
+	write(tmp_fd, line, ft_strlen(line));
+	free(line);
+	close(tmp_fd);
+	close(urandom_fd);
+	return (0);
+}
+
 int	get_infile(t_pipex *p_pipex, char **argv)
 {
 	if (true == p_pipex->here_doc)
 	{
-		handle_here_doc(); // TODO:
+		handle_here_doc(argv[2]);
 		p_pipex->in_fd = open(HERE_DOC_PATH, O_RDONLY);
 	}
 	else if (true == p_pipex->urandom)
 	{
 		handle_urandom(); // TODO:
-		p_pipex->in_fd = open(URANDOM_PATH, O_RDONLY);
+		p_pipex->in_fd = open(URANDOM_OUT_PATH, O_RDONLY);
 	}
 	else
 	{
