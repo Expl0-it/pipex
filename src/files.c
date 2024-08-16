d06c14b45f778c428dc4b142c718c1f2a0d3b2f7
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-16 13:43:12 +0200

    created a funct to handle here_doc parameter

--- src/files.c
+++ src/files.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 19:44:56 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/16 13:25:53 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/08/16 13:42:02 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,30 @@ int	read_line(char **line, int fd, char limiter)
 	buf[i + 1] = 0;
 	*line = buf;
 	return (i + 1);
+}
+
+int	handle_here_doc(char *limiter)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(HERE_DOC_PATH, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+		return (-1);
+	write(STDOUT_FILENO, "heredoc> ", 9);
+	while (read_line(&line, STDIN_FILENO, '\n') > 0)
+	{
+		if (ft_strncmp(line, limiter, ft_strlen(limiter) + 1))
+		{
+			free(line);
+			break ;
+		}
+		write(fd, line, ft_strlen(line));
+		free(line);
+		write(STDOUT_FILENO, "heredoc> ", 9);
+	}
+	close(fd);
+	return (0);
 }
 
 int	get_infile(t_pipex *p_pipex, char **argv)
