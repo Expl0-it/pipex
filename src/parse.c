b4c66c62a4224a8a20a01f4b661e537e9d83b949
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-16 14:20:42 +0200

    finished parse_args

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 19:27:13 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/09 20:09:32 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/08/16 14:19:36 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@ bool	parse_args(t_pipex *p_pipex, int argc, char **argv)
 		p_pipex->urandom = true;
 	if (argc < 5 + (int)p_pipex->here_doc)
 		return (false);
-	if ()
+	if (-1 == get_infile(p_pipex, argv))
+		return (false);
+	if (-1 == get_outfile(p_pipex, argv, argc))
+		return (false);
+	p_pipex->cmd_count = (argc - (3 + p_pipex->here_doc));
+	return (true);
 }
