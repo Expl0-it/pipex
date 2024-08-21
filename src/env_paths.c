6d9e07228727680475fc2bdf0b92472daaf323e9
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-21 19:27:38 +0200

    added a fn to get a particular string form envp

---
+++ src/env_paths.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_paths.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 19:07:13 by mamichal          #+#    #+#             */
+/*   Updated: 2024/08/21 19:22:20 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+// @return name of env variable passed as key or NULL if doesn't exist
+static char	*get_env(char *key, char **envp)
+{
+	int	i;
+	int	key_len;
+
+	key_len = ft_strlen(key);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(key, envp[i], key_len) == 0
+				 && envp[i][key_len] == '=')
+			return (ft_strchr(envp[i], '=') + 1);
+		i++;
+	}
+	return (NULL);
+}
+
