71e0260a3a2cdf8f04ae0b11e879af48b165f9d5
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-24 13:03:46 +0200

    added find_path fn

--- src/env_paths.c
+++ src/env_paths.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 19:07:13 by mamichal          #+#    #+#             */
-/*   Updated: 2024/08/21 19:22:20 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/08/24 13:03:24 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static char	*get_env(char *key, char **envp)
 	return (NULL);
 }
 
-char **get_paths(char **envp)
+char	**get_paths(char **envp)
 {
 	char	*path;
 
@@ -38,4 +38,33 @@ char **get_paths(char **envp)
 	if (NULL == path)
 		return (NULL);
 	return (ft_split(path, ':'));
+}
+
+char	*find_path(char *cmd, char **envp)
+{
+	int		i;
+	char	**paths;
+	char	*path;
+	char	*full_path;
+
+	if (0 == access(cmd, F_OK))
+		return (ft_substr(cmd, 0, ft_strlen(cmd)));
+	paths = get_paths(envp);
+	if (NULL == paths)
+		return (NULL);
+	i = -1;
+	while (paths[++i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		if (NULL == path)
+			return (free_arr(paths, -1), NULL);
+		full_path = ft_strjoin(path, cmd);
+		free(path);
+		if (NULL == full_path)
+			return (free_arr(paths, -1), NULL);
+		if (0 == access(full_path, F_OK))
+			return (free_arr(paths, -1), full_path);
+		free(full_path);
+	}
+	return (free_arr(paths, -1), NULL);
 }
